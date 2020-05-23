3a758d0c41d1d147527a190c5b6bd52488bcb585
jadlmir <elmirjad@gmail.com>
2020-05-23 13:43:06 -0400

    added material ui theme

---
+++ src/theme.tsx
@@ -0,0 +1,11 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+
+export const theme = createMuiTheme({
+    palette: {
+        background: {
+            default: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+        }
+    }
+})
+
