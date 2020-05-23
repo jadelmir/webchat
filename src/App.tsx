eb92e6642bfa2b24a4d5a4aac320ec5e885d8a9f
jadlmir <elmirjad@gmail.com>
2020-05-23 13:57:22 -0400

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
+import {Route , BrowserRouter as Router , Switch } from 'react-router-dom'
 
+const Login = ()=>{
+  return <h2>
+    login
+  </h2>
+}
+const Main = ()=>{
+  return <h2>
+    Main
+  </h2>
+}
+const NotFound = ()=> <h1>Page Not Found</h1>
 function App() {
   return (
     <div className="App">
-      hello world
-
+        <Router>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/" exact component={Main} />
+            <Route path="*" component={NotFound}/>
+          </Switch>
+        </Router>
     </div>
   );
 }
